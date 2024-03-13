b0703fa7f58edbbbf177a6c6d1d236bc1e8f61a0
sidducoder2002 <156557403+sidducoder2002@users.noreply.github.com>
2024-03-13 17:15:34 +0530

    Update ChiefMinister.java

--- src/main/java/com/taashee/resthibernapteapp/RestHibernateApp/entity/ChiefMinister.java
+++ src/main/java/com/taashee/resthibernapteapp/RestHibernateApp/entity/ChiefMinister.java
@@ -2,9 +2,7 @@ package com.taashee.resthibernapteapp.RestHibernateApp.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 
 @Entity
